38ea02c3c12bf78942fd1cbbfc462c27c6903500
fahemny <ffahemny@gmail.com>
2026-05-29 22:09:12 +0300

    ccccccccc

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL;
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const serviceRole = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
